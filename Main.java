afefbfcce5b6c512279ef48ba6c3ea2c82e5c00c
gLando23 <landen.gogel@evsck12.com>
2022-11-30 13:14:19 -0600

    Added break

--- Main.java
+++ Main.java
@@ -11,6 +11,7 @@ public class Main {
                switch(response){
                     case "Gogel":
                          gogel();
+                         break;
                     case "Bander":
                          bander();
                          break;
